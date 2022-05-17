c7afc2147b4254db46f8249c539602ba9f2738b3
Adam Lecznar <lecznara@gmail.com>
2022-05-17 16:24:58 -0400

    Completed Chapter Ten

--- Chp10.R
+++ Chp10.R
@@ -1,5 +1,7 @@
 # Chapter Ten: Estimation <codecell> setup
 library("lsr")
+library("sciplot")
+load("data/afl24.Rdata")
 # <codecell> the law of large numbers
 IQ <- rnorm(n = 10000,mean = 100, sd = 15)
 IQ <- round( IQ )
@@ -14,3 +16,38 @@ for (i in IQmean) {
 hist(IQmean,freq = FALSE)
 curve(dnorm(x,mean = 100,sd = 15),add = TRUE)
 # <codecell> Estimating Population Parameters
+# simulating sampling distributions of the standard deviation
+sd.2 <- matrix(rnorm(n = 100000,mean = 100,sd = 15),2,50000)
+sd.2 <- apply(sd.2,2,FUN = sd)
+hist(sd.2)
+abline(v = c(mean(sd.2),15))
+# <codecell> simulating the sample mean and sample standard deviations at different sample sizes
+ss <- c(1:10)
+mn <- ss
+std <- ss
+n <- data.frame(ss,mn,std)
+for (i in c(1:10)) {
+  norm <- matrix(rnorm(n = i * 10000,mean = 100,sd = 15),i,10000)
+  norm.mean <- colMeans(norm)
+  norm.sd <- apply(norm,2,FUN = sd)
+  n$mn[i] <- mean(norm.mean)
+  n$std[i] <- mean(norm.sd)
+}
+plot(x = n$ss,y = n$mn)
+plot(x = n$ss,y = n$std)
+# <codecell> estimating a confidence interval
+ciMean(x = afl$attendance)
+# plotting confidence intervals
+bargraph.CI(x.factor = year,    # found in sciplot package
+  response = attendance,
+  data = afl,
+  ci.fun = ciMean,
+  xlab = "Year",
+  ylab = "Average Attendance")
+lineplot.CI(    # found in sciplot package
+  x.factor = year,
+  response = attendance,
+  data = afl,
+  xlab = "Year",
+  ylab = "Average Attendance"
+)
